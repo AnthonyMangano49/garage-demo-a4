0e9906a419b8c3310742cba614cfd1ed96a3f0de
AnthonyMangano49 <32198206+AnthonyMangano49@users.noreply.github.com>
2017-09-25 20:35:13 +0300

    components seperated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,54 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    styles: [`
-        #title-container {
-            font-size: 1.5em;
-            text-align: center;
-            text-transform: uppercase;
-            padding: 25px;
-            background: #8d9bff;
-            color: #eff1ff;
-        }
-        #title-container span{
-            cursor: pointer;
-        }
-        #router-container {
-            display: flex;
-            flex-direction: row;
-        }
-        #router-container a {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            flex-grow: 1;
-            background: white;
-            color: #8d9bff;
-            text-transform: uppercase;
-            height: 3em;
-            text-decoration: none;
-        }
-        #router-container a:hover {
-            background: grey;
-            color: white;
-        }
-        @media(min-width: 600px){
-            #title-container{
-                font-size: 3em;
-            }
-        }
-    `],
-    template: `
-        <div id="title-container">
-            <span routerLink="/home">{{title}}</span>
-        </div>
-        <div id="router-container">
-            <a routerLink="/cars/all">All Cars</a>
-            <a routerLink="/cars/available">Available</a>
-            <a routerLink="/cars/unavailable">Unavailable</a>
-        </div>
-        <router-outlet></router-outlet>
-    `
+    styleUrls: ['./app.component.css'],
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
     title = 'Jalapeno\'s Garage';
